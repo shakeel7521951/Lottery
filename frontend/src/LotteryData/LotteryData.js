e1eb9bfa26fb2921f55f03db77985c4ec7bbabbd
Muhammad Shakeel <149057002+shakeel7521951@users.noreply.github.com>
2024-10-20 09:32:38 +0500

    Update LotteryData.js

--- frontend/src/LotteryData/LotteryData.js
+++ frontend/src/LotteryData/LotteryData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const fetchLotteries = async () => {
     try {
-        const response = await axios.get("http://localhost:6005/get-lottery-data");
+        const response = await axios.get("https://lottery-three-ivory.vercel.app/get-lottery-data");
         return response.data;
     } catch (error) {
         throw error
