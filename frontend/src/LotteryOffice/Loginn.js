b58bbacc02b59710e2869ce35a0a7fbe2611e2e3
Muhammad Shakeel <149057002+shakeel7521951@users.noreply.github.com>
2024-10-20 09:36:23 +0500

    Update Loginn.js

--- frontend/src/LotteryOffice/Loginn.js
+++ frontend/src/LotteryOffice/Loginn.js
@@ -23,7 +23,7 @@ function Loginn() {
         };
 
         try {
-            const response = await fetch("http://localhost:6005/login", {
+            const response = await fetch("https://lottery-three-ivory.vercel.app/login", {
                 method: 'POST',
                 credentials: 'include', 
                 headers: {
